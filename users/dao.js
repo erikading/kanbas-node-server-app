781b3b0fb1b3dd6a6514cc9c4c63830978c14095
Erika Ding <erikajding@gmail.com>
2023-12-04 00:03:44 -0500

    a6 Mongo fa23

---
+++ users/dao.js
@@ -0,0 +1,11 @@
+import model from "./model.js";
+export const createUser = (user) => model.create(user);
+export const findAllUsers = () => model.find();
+export const findUserById = (userId) => model.findById(userId);
+export const findUserByUsername = (user) =>
+  model.findOne({ username: user });
+export const findUserByCredentials = (user, pass) =>
+  model.findOne({ username: user, password: pass });
+export const updateUser = (userId, user) =>
+  model.updateOne({ _id: userId }, { $set: user })
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
